bbb933f1fa64456133d24c67e4cf4acd8784c3e8
James Morical <77235874+jkmor31@users.noreply.github.com>
2021-12-23 11:15:02 -0600

    make edit in place

--- src/app/fund/fund.component.ts
+++ src/app/fund/fund.component.ts
@@ -11,6 +11,8 @@ import { Fund } from './fund.model';
 })
 export class FundComponent implements OnInit {
   fund: Fund = {};
+  tempFundList: any[][] = [];
+  key:any;
 
   constructor(
     private route:ActivatedRoute, 
@@ -23,6 +25,10 @@ export class FundComponent implements OnInit {
       const myid = +params['id'];
       this.fundService.getFund(myid).subscribe(payload=>{
         this.fund = payload;
+        for(const [key, value] of Object.entries(this.fund)){
+          const localArray = [key, value, false];
+          this.tempFundList.push(localArray);
+        }
       })
     })
   }
@@ -30,8 +36,31 @@ export class FundComponent implements OnInit {
   deleteFund():void {
     this.fundService.deleteFund(this.fund.id).subscribe();
     alert("This fund has been deleted");
+    this.router.navigateByUrl("/funds");
   }
   edit():void {
-   this.router.navigateByUrl(`/funds/${this.fund.id}/edit`); 
+    this.reconstituteFund();
+    this.fundService.updateFund(this.fund).subscribe(data=>{
+      if(data) {
+        this.router.navigateByUrl("/funds");
+      }
+    })
+  }
+  
+  
+  reconstituteFund():void {
+    const reconstitutedFund: any = {};
+    this.tempFundList.forEach(item =>{
+      reconstitutedFund[item[0]] = item[1];
+    })
+    this.fund = reconstitutedFund;
+  }
+
+  makeEditable(index:number): void {
+    this.tempFundList[index][2] = true;
+  }
+
+  makeStatic(index: number): void {
+    this.tempFundList[index][2] = false;
   }
 }
